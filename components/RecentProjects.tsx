77df0db5b5c9b30fbb2c5d8339ff5622a5b86c12
Muhamad Hanif Hafizhan <muhamadhanifhafizhan@gmail.com>
2025-04-09 09:55:14 +0700

    feat : added sentry.io

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -15,7 +15,7 @@ const RecentProjects = () => {
             key={id}
             className="lg:min-h-[32.5rem] sm:h-[41rem] h-[32rem] flex items-center justify-center sm:w-[570px] w-[80vw]"
           >
-            <PinContainer title={title} href={link}>
+            <PinContainer title="ui.aceternity.com" href={link}>
               <div className="relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden sm:h-[40vh] h-[30vh] mb-10">
                 <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#bfc4e8]">
                   <img src="/bg.png" alt="bg-img" />
