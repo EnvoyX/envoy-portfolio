5e02122baf57717078a99b81e2990ce73457bd76
Muhamad Hanif Hafizhan <muhamadhanifhafizhan@gmail.com>
2025-04-11 11:51:34 +0700

    chore : Experiences section in progress

---
+++ components/Experience.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Experience = () => {
+  return (
+    <section className="py-20" id="testimonials">
+      <h1 className="heading">
+        My {""}
+        <span className="text-purple">experiences</span>
+      </h1>
+      <div className="w-full mt-12 grid lg:grid-cols-4 grid-cols-1 gap-10"></div>
+    </section>
+  );
+};
+
+export default Experience;
