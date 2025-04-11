c6366101b0f0c6ac57a86f151ba7555fd2ade7a1
Muhamad Hanif Hafizhan <muhamadhanifhafizhan@gmail.com>
2025-04-11 23:26:33 +0700

    fix : fix some bugs

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,11 @@
 import { FaLocationArrow } from "react-icons/fa6";
 import MagicButton from "./ui/MagicButton";
 import { socialMedia } from "@/data";
+import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="w-full pt-20 pb-10">
+    <footer className="w-full pb-10 mb-[100px] md:mb-5">
       <div className="w-full absolute left-0 -bottom-72 min-h-96">
         <img
           src="/footer-grid.svg"
@@ -20,13 +21,13 @@ const Footer = () => {
         <p className="text-white-200 md:mt-10 my-5 text-center">
           I would love to help build & develop your dream web app
         </p>
-        <a href="mailto:muhamadhanifhafizhan@gmail.com">
+        <Link href="mailto:muhamadhanifhafizhan@gmail.com">
           <MagicButton
             title="Contact Me!"
             icon={<FaLocationArrow />}
             position="left"
           />
-        </a>
+        </Link>
       </div>
       <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
         <p className="md:text-base text-sm md:font-normal font-light">
